f36795d0d5b084e0a0844213fc1a5492a15e7d00
Ola Nyström <ola@ofelbar.se>
2020-12-14 16:06:52 +0100

    Reimplement day05p1

--- 2020/day05/day05p1.py
+++ 2020/day05/day05p1.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python
 import os
 import sys
+import time
 with open(os.path.join(sys.path[0],'day05.data')) as fp:
    lines = [x.strip() for x in fp.readlines()]
 
+starttime=time.time()
 
 maxid = 0
 for line in lines:
-    maxrow = 127
-    minrow = 0
-    maxcol = 7
-    mincol = 0
+    maxrow = 127; minrow = 0
+    maxcol = 7;   mincol = 0
     for num,ltr in enumerate(line):
-        if num < 7:
-            if(ltr == "F"): # 32 - 63 => 32 - 47 
-                maxrow = maxrow-(((maxrow+1)-minrow)/2)
-            elif(ltr == "B"):  #  32-47 => 40-47
-                minrow = minrow + (((maxrow+1)-minrow)/2)
-        else:
-            if(ltr == "L"): # 32 - 63 => 32 - 47 
-                maxcol = maxcol-(((maxcol+1)-mincol)/2)
-            elif(ltr == "R"):  #  32-47 => 40-47
-                mincol = mincol + (((maxcol+1)-mincol)/2)
+        midrow = maxrow-(((maxrow+1)-minrow)/2)
+        midcol = maxcol-(((maxcol+1)-mincol)/2)
+        if(ltr == "F"): 
+            maxrow = midrow
+        elif(ltr == "B"):
+            minrow = midrow
+        elif(ltr == "L"): 
+            maxcol = midcol
+        elif(ltr == "R"): 
+            mincol = midcol
     # print(f"row: {maxrow} ({minrow})\tcol: {maxcol} ({mincol} ID: {(maxrow*8)+maxcol}")
     id = (maxrow*8)+maxcol
-    
-    if id > maxid:
-        maxid = id
+    maxid = max(id,maxid)
 
 print(maxid)
+print(f"Time: {(time.time()-starttime)*1000:0.4}ms")
