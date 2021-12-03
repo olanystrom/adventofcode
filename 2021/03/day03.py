c984b6f13c14ddc01b23e5812926d02ea62779b7
Ola Nyström <ola.nystrom@konsult.atg.se>
2021-12-03 13:12:07 +0100

    Day03 part 2

--- 2021/03/day03.py
+++ 2021/03/day03.py
@@ -29,12 +29,38 @@ def part1(input: List) -> int:
     print(ubits, epsilon)
     return gamma*epsilon
 
-def part2(input: List) -> int:
-    return None
+
+
+def part2(linput: List) -> int:
+    oxylist = linput.copy()
+    co2list = linput.copy()
+    oxycompare = []
+    co2compare = []
+    for n in range(len(linput[0])):
+    
+        if sum([int(a[n:n+1]) for a in oxylist]) >= len(oxylist)/2:
+            oxycompare.append('1')
+        else:
+            oxycompare.append('0')
+    
+        if sum([int(a[n:n+1]) for a in co2list]) >= len(co2list)/2:
+            co2compare.append('0')
+        else:
+            co2compare.append('1')
+    
+        oxylist = [a for a in oxylist if list(a[:n+1]) == oxycompare]
+        co2list = [a for a in co2list if list(a[:n+1]) == co2compare]
+    
+        if len(oxylist) == 1:
+            oxygen = int("".join([str(a) for a in oxylist]),2)
+        if len(co2list) == 1:
+            co2 = int("".join([str(a) for a in co2list]),2)
+    return oxygen*co2
 
 assert part1(TEST) == 198
-#assert part2(TEST) == 900
+assert part2(TEST) == 230
 # print("Part 1 TEST", part1(TEST))
+# print("Part 2 TEST", part2(TEST))
 print("Part 1", part1(lines))
 print("Part 2", part2(lines))
 AOC.printTimeTaken()
