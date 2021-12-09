5794150eda87e59d7db04abedc4330a5a32dc610
Ola Nyström <ola.nystrom@konsult.atg.se>
2021-12-09 22:24:16 +0100

    Day09 part2

--- 2021/09/day.py
+++ 2021/09/day.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, ParamSpecArgs
 import sys
+import math
 sys.path.insert(1, sys.path[0] + '/..')  # Add parent of my directory to path
 import AOC  ## pylint: disable=import-error
 lines: List = AOC.loadInput(None,'input', notInt=True)
@@ -26,19 +27,45 @@ def part1(linput: List) -> int:
       for y in range(len(linput[0])):
          # print(f"{x=},{y=}")
          if lowpoint(linput, x, y):
+            print(f"{x=},{y=}")
             risktot += int(linput[x][y]) + 1
                # print(f"{x=} {y=} {linput[x][y]=}")
    return risktot
       
 
-def part2(linput: List) -> int:
-   return 0
+def floodfill(matrix, x: int , y: int, areacounter: set):
+   if (x,y) in areacounter:
+      return areacounter
+   if matrix[x][y] < "9":
+      areacounter.add((x,y))
+   
+   #recursively invoke flood fill on all surrounding cells:
+      if x > 0:
+         areacounter = floodfill(matrix,x-1,y, areacounter)
+      if x < len(matrix) - 1:
+         areacounter = floodfill(matrix,x+1,y, areacounter)
+      if y > 0:
+         areacounter = floodfill(matrix,x,y-1, areacounter)
+      if y < len(matrix[0]) - 1:
+         areacounter = floodfill(matrix,x,y+1, areacounter)
+   return areacounter
 
+def part2(linput: List) -> int:
+   sizes = []
+   for x in range(len(linput)):
+      for y in range(len(linput[0])):
+         if lowpoint(linput, x, y):
+            # print(f"{x=},{y=}")
+            localarea = set()
+            localarea = floodfill(linput, x, y, localarea)
+            # print(localarea)
+            sizes.append(len(localarea))
+   return math.prod(sorted(sizes)[-3:])
 
 # assert part1(TEST) == 15
 # assert part2(TEST) == x
 # print("Part 1 TEST (15)", part1(TEST))
-# print("Part 2 TEST (x)", part2(TEST))
-print("Part 1", part1(lines))
-# print("Part 2", part2(lines))
+# print("Part 2 TEST (1134)", part2(TEST))
+# print("Part 1", part1(lines))
+print("Part 2", part2(lines))
 AOC.printTimeTaken()
